400254a62ea10048cb4e00f9066b21b7e98568aa
Evandro Oliveira de Novais <evandro-novais@outlook.com>
2023-08-14 11:58:11 -0300

    GET game by id

--- src/main/java/com/eontecnologia/dslist/services/GameService.java
+++ src/main/java/com/eontecnologia/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import com.eontecnologia.dslist.dto.GameDTO;
 import com.eontecnologia.dslist.dto.GameMinDTO;
 import com.eontecnologia.dslist.entit‭ies.Game;
 import com.eontecnologia.dslist.repositories.GameRepository;
@@ -15,9 +17,17 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
+	@Transactional(readOnly = true)
 	public List<GameMinDTO> findAll(){		
 		
 		List<Game> result = gameRepository.findAll();
 		return result.stream().map(x -> new GameMinDTO(x)).toList();
+	}
+	
+	@Transactional(readOnly = true)
+	public GameDTO findById(Long id) {
+		
+		Game result = gameRepository.findById(id).get();
+		return new GameDTO(result);		
 	}
 }
